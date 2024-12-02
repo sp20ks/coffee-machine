45ea4908b7786b347d7e4492a2e2b6a147d4b047
Спасскова Ксения <ksenia@MacBook-Pro.local>
2024-12-02 22:29:44 +0300

    added spi

---
+++ spi.c
@@ -0,0 +1,50 @@
+#include "spi.h"
+
+// Инициализация SPI как мастер
+void SPI_MasterInit(void) {
+    DDRB |= (1 << SCK_BIT) | (1 << MOSI_BIT) | (1 << SS_BIT); // SCK, MOSI, SS - выходы
+    DDRB &= ~(1 << MISO_BIT);                                // MISO - вход
+    PORTB |= (1 << SS_BIT);                                  // Деактивируем слейва (SS высокий уровень)
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);           // SPI включен, мастер, f/16
+}
+
+// Инициализация SPI как слейв
+void SPI_SlaveInit(void) {
+    DDRB |= (1 << MISO_BIT);  // MISO - выход
+    DDRB &= ~((1 << SCK_BIT) | (1 << MOSI_BIT) | (1 << SS_BIT)); // SCK, MOSI, SS - входы
+    SPCR = (1 << SPE) | (1 << SPIE); // Включаем SPI и прерывания
+    sei(); // Разрешаем прерывания
+}
+
+// Функция передачи данных по SPI
+uint8_t SPI_Transfer(uint8_t data) {
+    SPDR = data;                            // Запускаем передачу данных
+    while (!(SPSR & (1 << SPIF)));          // Ждем завершения
+    return SPDR;                            // Возвращаем принятый байт
+}
+
+// Процедура запроса массива данных от слейва
+void SPI_RequestArray(uint8_t *data) {
+    PORTB &= ~(1 << SS_BIT); // Активируем слейва
+    SPI_Transfer(START_COMMAND); // Запрос на получение данных
+    _delay_ms(10);
+
+    for (uint8_t i = 0; i < ARRAY_SIZE; ++i) {
+        _delay_ms(10); // Небольшая задержка для стабильности
+        data[i] = SPI_Transfer(START_COMMAND); // Сохраняем данные
+    }
+
+    PORTB |= (1 << SS_BIT); // Деактивируем слейва
+}
+
+// Прерывание для отправки данных
+ISR(SPI_STC_vect) {
+    if (SPDR == START_COMMAND) {
+        if (index_data >= ARRAY_SIZE) {
+            index_data = 0;
+            return;
+        }
+        SPDR = data[index_data];
+        ++index_data;
+    }
+}
---
+++ spi.h
@@ -0,0 +1,24 @@
+#ifndef SPI_H
+#define SPI_H
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define ARRAY_SIZE 5  // Размер массива
+#define SCK_BIT  7
+#define MISO_BIT 6
+#define MOSI_BIT 5
+#define SS_BIT   4
+#define START_COMMAND 0x10
+
+void SPI_MasterInit(void); // Инициализация SPI как мастер
+void SPI_SlaveInit(void); // Инициализация SPI как слейв
+uint8_t SPI_Transfer(uint8_t data); // Функция передачи данных по SPI
+void SPI_RequestArray(uint8_t *data); // Процедура запроса массива данных от слейва
+ISR(SPI_STC_vect); // Прерывание для отправки данных
+
+extern uint8_t data[ARRAY_SIZE]; // Массив для передачи по SPI
+extern uint8_t index_data; // Индекс текущего значения в массиве для передачи по SPI
+
+#endif
